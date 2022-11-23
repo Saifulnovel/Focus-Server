cfc6efcc1039cdd51a7a2b8fe257354987cdcdda
Saifulnovel <sinovel07@gmail.com>
2022-11-23 21:50:50 +0600

    listen-option-add

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Camera Resale Server is running')
+})
 
+app.listen(port, () => {
+    console.log(`Camera Resale Server is running at ${port}`);
+})
